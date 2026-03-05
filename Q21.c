035c049912663df8058e03abddc9c9a96b27657e
RudrakshPant <pantrudraksh5@gmail.com>
2026-03-06 02:01:05 +0530

    Create Q21.c

---
+++ Q21.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+  int data;
+  struct node *next;
+} node;
+node *createnode(int data){
+  node *newnode = malloc(sizeof(node));
+  newnode->data = data;
+  newnode->next = NULL;
+  return newnode;
+}
+int main()
+{
+  int i, n, val;
+  printf("Enter the number of nodes: ");
+  scanf("%d", &n);
+  node *head = NULL;
+  node *temp = NULL;
+  for(i=0; i<n; i++)
+  { printf("Enter element at node %d: ", i+1);
+    scanf("%d", &val);
+    node *link = createnode(val);
+    if(head == NULL)
+    { head = link;
+      temp = head; }
+    else
+    { temp->next = link;
+      temp = link; }
+  }
+  node *print = head;
+  printf("Linked List:\n");
+  for(i=0; i<n; i++)
+  { printf("%d ", print->data);
+    print = print->next;
+  }
+  return 0;
+}
